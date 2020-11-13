ed42ec6c1b1635d5dc0e926a79ce23b7f49a7879
kuanhungchen <khchen.me@gmail.com>
2020-11-13 11:01:08 +0800

    update hw2 code

--- hw2_109061539/src/model.py
+++ hw2_109061539/src/model.py
@@ -11,47 +11,120 @@ class AutoEncoder(nn.Module):
             nn.Conv2d(1, 16, 3),
             nn.ReLU(),
             nn.Dropout(0.25),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(2, 2),
             nn.Conv2d(16, 8, 3),
             nn.ReLU(),
             nn.Dropout(0.25),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2, 2)
         )
         self.nrml_encoder = nn.Sequential(
             nn.Conv2d(1, 16, 3),
             nn.ReLU(),
             nn.Dropout(0.25),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(2, 2),
             nn.Conv2d(16, 8, 3),
             nn.ReLU(),
             nn.Dropout(0.25),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2, 2)
         )
         self.dfct_encoder = nn.Sequential(
             nn.Conv2d(1, 16, 3),
             nn.ReLU(),
             nn.Dropout(0.25),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(2, 2),
             nn.Conv2d(16, 8, 3),
             nn.ReLU(),
             nn.Dropout(0.25),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.bdry_decoder = nn.Sequential(
+            # nn.UpsamplingNearest2d(scale_factor=2),
+            # nn.MaxUnpool2d(2, stride=2),
+            nn.ConvTranspose2d(8, 16, 4, stride=2),
+            nn.ReLU(),
+            # nn.UpsamplingNearest2d(scale_factor=2),
+            # nn.MaxUnpool2d(2, stride=2),
+            nn.ConvTranspose2d(16, 1, 4, stride=2),
+            nn.ReLU(),
+            # nn.UpsamplingNearest2d(scale_factor=2),
+        )
+        self.nrml_decoder = nn.Sequential(
+            # nn.UpsamplingNearest2d(scale_factor=2),
+            # nn.MaxUnpool2d(2, stride=2),
+            nn.ConvTranspose2d(8, 16, 4, stride=2),
+            nn.ReLU(),
+            # nn.UpsamplingNearest2d(scale_factor=2),
+            # nn.MaxUnpool2d(2, stride=2),
+            nn.ConvTranspose2d(16, 1, 4, stride=2),
+            nn.ReLU(),
+            # nn.UpsamplingNearest2d(scale_factor=2),
+        )
+        self.dfct_decoder = nn.Sequential(
+            # nn.UpsamplingNearest2d(scale_factor=2),
+            # nn.MaxUnpool2d(2, stride=2),
+            nn.ConvTranspose2d(8, 16, 4, stride=2),
+            nn.ReLU(),
+            # nn.UpsamplingNearest2d(scale_factor=2),
+            # nn.MaxUnpool2d(2, stride=2),
+            nn.ConvTranspose2d(16, 1, 4, stride=2),
+            nn.ReLU(),
+            # nn.UpsamplingNearest2d(scale_factor=2),
         )
     
-    def forward(self, data):
+    def encode(self, data):
         """
         Args:
             data.shape = (batch_size, 3 26, 26)
+
+        Return:
+            latents.shape = (batch_size, 24, 5, 5)
         """
 
         bdry = data[:, 0, :, :].view(-1, 1, 26, 26)
         nrml = data[:, 1, :, :].view(-1, 1, 26, 26)
         dfct = data[:, 2, :, :].view(-1, 1, 26, 26)
         
-        bdry_logits = self.bdry_encoder(bdry)
-        nrml_logits = self.nrml_encoder(nrml)
-        dfct_logits = self.dfct_encoder(dfct)
+        bdry_latent = self.bdry_encoder(bdry)
+        nrml_latent = self.nrml_encoder(nrml)
+        dfct_latent = self.dfct_encoder(dfct)
         
-        logits = np.stack((bdry_logits, nrml_logits, dfct_logits), axis=1)
-        return logits
+        latent = torch.cat((bdry_latent, nrml_latent, dfct_latent), 1)
+
+
+        return latent
+
+    def decode(self, latent):
+        """
+        Args:
+            latents.shape = (batch_size, 24, 5, 5)
+
+        Return:
+            data.shape = (batch_size, 3, 26, 26)
+        """
+        bdry_latent = latent[:, 0:8, :, :].view(-1, 8, 5, 5)
+        nrml_latent = latent[:, 8:16, :, :].view(-1, 8, 5, 5)
+        dfct_latent = latent[:, 16:24, :, :].view(-1, 8, 5, 5)
+
+        bdry = self.bdry_decoder(bdry_latent)
+        nrml = self.nrml_decoder(nrml_latent)
+        dfct = self.dfct_decoder(dfct_latent)
+
+        data = torch.cat((bdry, nrml, dfct), 1)
+        
+        return data
+
+    def loss(self, data, reconstructed_data):
+        """
+        Args:
+            data.shape = (batch_size, 3, 26, 26)
+            reconstructed_data.shape = (batch_size, 3, 26, 26)
+        Return:
+            loss.shape = ()
+        """
+        
+        loss_function = nn.MSELoss()
+        loss = loss_function(data, reconstructed_data)
+
+        return loss
 
--- hw2_109061539/src/train.py
+++ hw2_109061539/src/train.py
@@ -8,18 +8,43 @@ from model import AutoEncoder as AE
 def train(path_to_data, path_to_checkpoints=""):
 
     dataset = Dataset(path_to_data)
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
     model = AE()
     model = model.float()
+    model = model.cuda()
 
-    optimizer = optim.SGD(model.parameters(), lr=3e-4)
+    optimizer = optim.SGD(model.parameters(), lr=1e-3)
+    
+    epoch_num = 1000
+    for epoch_idx in range(1, epoch_num + 1):
+        total_loss = []
+        for batch_idx, (data, label) in enumerate(dataloader):
+            data = data.float()
+            data = data.cuda()
 
-    for batch_idx, (datas, labels) in enumerate(dataloader):
-        logits = model.train().forward(datas.float())
-        print(logits.shape)
-        
-        break
+            label = label.float()
+            label = label.cuda()
+
+            latent = model.train().encode(data)
+            latent = latent.float()
+
+            # print("latent.shape ==>", latent.shape)
+
+            reconstructed_data = model.train().decode(latent)
+            reconstructed_data = reconstructed_data.float()
+
+            # print("reconstructed_data.shape ==>", reconstructed_data.shape)
+            
+            loss = model.loss(data, reconstructed_data)
+            
+            total_loss.append(loss)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print("Epoch: {:2d} | Loss: {:.4f}".format(epoch_idx, sum(total_loss) / len(total_loss)))
+
 
 if __name__ == "__main__":
     train(path_to_data="wafer")
